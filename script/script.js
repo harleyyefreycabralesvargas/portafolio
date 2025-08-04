e6e821d9171c4adc1e604e8eb0f410ce2132a9c2
harley <arleycabralesvargas@gmail.com>
2025-08-04 15:31:31 -0500

    feat: :fire: Advance personal information

--- script/script.js
+++ script/script.js
@@ -1,6 +1,1 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const toggleBtn = document.getElementById("modo-toggle");
-  toggleBtn.addEventListener("click", () => {
-    document.body.classList.toggle("dark-mode");
-  });
-});
+
